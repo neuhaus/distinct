9b1a747680f7b1dff83416d31be14155545a2d24
S. Neuhaus <neuhaus@users.noreply.github.com>
2024-10-30 22:27:12 +0100

    Untested Mastodon version.

--- fill_cache.py
+++ fill_cache.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 '''
-    fill cache of known urls from previous tweets for twitter deduplication bot
-    Copyright (C) 2016 Sven Neuhaus
+    fill cache of known urls from previous toots for Mastodon deduplication bot
+    Copyright (C) 2024 Sven Neuhaus
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as published
@@ -16,81 +16,82 @@
 
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-'''
+ '''
 
 import configparser
-from tweepy import OAuthHandler
-import tweepy
 import shelve
 import requests
 import sys
-import codecs
 import datetime
+from mastodon import Mastodon
 
-user = None
-api = None
 session = None
+api = None
+user = None
 
-
-def cache_tweet_urls(cache_filename, tweetcount):
-    tweets = 0
-    tweet_duplicate_url = 0
-    tweet_nourl = 0
-    """ Stores URLs found in past tweets in the cache """
+def cache_toot_urls(cache_filename, tootcount):
+    toots = 0
+    toot_duplicate_url = 0
+    toot_nourl = 0
+    """ Stores URLs found in past toots in the cache """
     seen_urls = shelve.open(filename=cache_filename, writeback=True)
-    statuses = api.user_timeline(user_id=user.id_str, count=tweetcount,
-                                 include_rts=False)
-    for tweet in statuses:
-        print("\n\ntweet = %s" % tweet)
-        tweets += 1
-        print("created at %s" % datetime.datetime.fromtimestamp(int(tweet.created_at.timestamp())))
-        if not tweet.entities.get("urls"):
-            tweet_nourl += 1
+    statuses = api.timeline_user(user.id, limit=tootcount)  # Fetch user timeline
+
+    for toot in statuses:
+        print("\n\ntoot = %s" % toot)
+        toots += 1
+        print("created at %s" % toot['created_at'])
+        
+        if not toot.get('media_attachments'):
+            toot_nourl += 1
             continue
-        for urldata in tweet.entities['urls']:
-            if seen_urls.get(urldata['expanded_url']):
-                print("url already seen %s" % urldata['expanded_url'])
-                tweet_duplicate_url += 1
-                continue
-            seen_urls[urldata['expanded_url']] = int(
-                tweet.created_at.timestamp())
-            redirected_url = session.head(
-                urldata['expanded_url'], allow_redirects=True).url
-            if seen_urls.get(redirected_url):
-                print("redirected_url already seen %s" %
-                      urldata['expanded_url'])
-                tweet_duplicate_url += 1
-                continue
-            seen_urls[redirected_url] = int(tweet.created_at.timestamp())
-            print("expanded url %s and\nredirected url %s" %
-                  (urldata['expanded_url'], redirected_url))
+        
+        for media in toot['media_attachments']:
+            if media['type'] == 'image' and 'url' in media:
+                url = media['url']
+                
+                if seen_urls.get(url):
+                    print("URL already seen %s" % url)
+                    toot_duplicate_url += 1
+                    continue
+                
+                seen_urls[url] = int(toot['created_at'].timestamp())
+                redirected_url = session.head(url, allow_redirects=True).url
+                
+                if seen_urls.get(redirected_url):
+                    print("Redirected URL already seen %s" % url)
+                    toot_duplicate_url += 1
+                    continue
+                
+                seen_urls[redirected_url] = int(toot['created_at'].timestamp())
+                print("Expanded URL %s and redirected URL %s" % (url, redirected_url))
+    
     seen_urls.sync()
     seen_urls.close()
-    print("%d tweets %d duplicate URLs %d without URL" %
-          (tweets, tweet_duplicate_url, tweet_nourl))
+    print("%d toots, %d duplicate URLs, %d without URL" % (toots, toot_duplicate_url, toot_nourl))
 
 
 def main():
-    sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
     config = configparser.ConfigParser()
-    if len(sys.argv) == 2:     # alternate config file
+    if len(sys.argv) == 2:  # Alternate config file
         config.read(sys.argv[1], encoding='utf8')
     else:
         config.read('distinct.ini', encoding='utf8')
+    
     default = config['DEFAULT']
-    auth = OAuthHandler(default['consumer_key'],
-                        default['consumer_secret'])
-    auth.set_access_token(default['access_token'],
-                          default['access_token_secret'])
     global api
-    api = tweepy.API(auth)
+    api = Mastodon(
+        access_token=default['access_token'],
+        api_base_url=default['api_base_url']
+    )
+    
     global user
-    user = api.get_user(screen_name=default['follow_user'])
-    print("user " + default['follow_user'] + " id_str " + user.id_str)
+    user = api.account_verify_credentials()
+    print("User '%s' id %d" % (user['username'], user['id']))
+    
     global session
-    session = requests.Session()     # so connections are recycled
-    cache_tweet_urls("urlcache.{}.db".format(default['follow_user']), 1500)
-
+    session = requests.Session()  # Recycle connections
+    
+    cache_toot_urls("urlcache.{}.db".format(user['username']), 1500)
 
 main()
